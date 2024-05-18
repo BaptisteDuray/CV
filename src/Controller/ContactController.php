3e7e2fc412cf6b62e43763738f1666af6817ac9a
BaptisteDuray <baptiste.duray@oclock.school>
2024-05-18 17:41:37 +0200

    CV

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $contactForm = $this->createForm(\App\Form\ContactFormType::class);
+        $contact = new Contact();
+        $contactForm = $this->createForm(ContactFormType::class, $contact);
+
+        $contactForm->handleRequest($request);
+
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+            // Enregistrer les données dans la base de données
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            // Ajouter un message flash
+            $this->addFlash('success', 'Your message has been sent, I will answer you soon.');
+
+            // Rediriger pour éviter la resoumission du formulaire
+            return $this->redirectToRoute('app_contact');
+        }
 
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
